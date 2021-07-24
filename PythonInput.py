4f38d4867a2c83c299c298f5c81502c6242e01d1
Guillaume VdS <guillaume.vds.97@gmail.com>
2021-07-25 00:17:33 +0100

    making requested changes as per PR

--- PythonInput.py
+++ PythonInput.py
@@ -1,23 +1,24 @@
-#Script taking input from user
+"""Script taking input from user"""
 
-max_buffer_length = 512
+MAX_USER_INPUT_BUFFER_LENGTH = 512
 
-# Will take user input, checks if input does not exceed 512 characters
 def check_for_input():
-    # This loop will execute for as long as input is invalid.
-    # Will return
-    while 1:
-        buffer = input("Please enter input here: ")
-        if(len(buffer) <= max_buffer_length):
-            return buffer
+    """ Will take user input, checks if input does not exceed 512 characters
+    This loop will execute for as long as input is invalid.
+    Will return input through buffer variable
+    """
+    while True:
+        input_buffer = input("Please enter input here: ")
+        if(len(input_buffer) <= MAX_USER_INPUT_BUFFER_LENGTH):
+            return input_buffer
         else:
-            print("Buffer overflow")
+            print("Current input exceeds maximum allowed characters.")
 
 
 
 def main():
-    # Executes an infinite loop, to continiously execute below functions
-    while 1:
+    """Executes an infinite loop, to continiously execute below functions"""
+    while True:
         main_buffer = check_for_input()
         print('returning:', main_buffer)
 
