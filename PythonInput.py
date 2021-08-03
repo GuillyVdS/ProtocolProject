0cdd4e2f7d3e295669e944f4f10999e828104a0d
Guillaume VdS <guillaume.vds.97@gmail.com>
2021-08-03 12:57:09 +0100

    cleaning up send_data_over_serial function and adding spaces between parentheses

--- PythonInput.py
+++ PythonInput.py
@@ -14,20 +14,19 @@ def check_for_input():
     """
 
     while True:
-        input_buffer = input("Please enter input here: ")
-        if(len(input_buffer) <= MAX_USER_INPUT_BUFFER_LENGTH):
+        input_buffer = input( "Please enter input here: " )
+        if( len( input_buffer ) <= MAX_USER_INPUT_BUFFER_LENGTH ):
             return input_buffer
         else:
-            print("Current input exceeds maximum allowed characters.")
+            print( "Current input exceeds maximum allowed characters." )
 
-def send_data_over_serial(serial_port: serial.Serial, data_to_send: str):
+def send_data_over_serial( serial_port: serial.Serial, data_to_send: str ):
     """ This function takes input from the buffer in main, converts this to
     utf-8. The serial port is opened and the converted data will
     be written to it.
     """
 
-    data_as_bytes = bytes(data_to_send, 'ascii')
-    serial_port.write(data_as_bytes)
+    serial_port.write( bytes( data_to_send, 'ascii' ) )
 
 def main():
     """Initialising a serial port object and setting the correct BAUD rate."""
@@ -39,13 +38,13 @@ def main():
 
         while True:    #Executes an infinite loop, to repeat below functions
             main_buffer = check_for_input()
-            send_data_over_serial(serial_port_object, main_buffer)
+            send_data_over_serial( serial_port_object, main_buffer )
 
     except serial.SerialException as e:
-        print("Error configuring Serial port")
-        print(e)
+        print( "Error configuring Serial port" )
+        print( e )
     except Exception as e:
-        print("Failed to perform action")
+        print( "Failed to perform action" )
     #except KeyBoardInterrupt:
         serial_port.close()
         quit()
