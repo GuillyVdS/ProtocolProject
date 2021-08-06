2adecd31a82721b43d0c62ea6e578e0816c8882c
Guillaume VdS <guillaume.vds.97@gmail.com>
2021-08-06 20:06:46 +0100

    Added C file which listens for a message coming throug a socket

---
+++ C_Receive.c
@@ -0,0 +1,40 @@
+/*
+    -Create an application in C, that will forever run.
+    -This application should be able to receive messages from the COM/Serial/virtualSOCAT port,
+    print it to the command line, then return back to waiting for information to come across the port.
+    -This can be tested using "echo"
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+// Linux headers needed for serial receive
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <errno.h> // Error integer and strerror() function
+#include <unistd.h> // write(), read(), close()
+
+
+#define BUFFERSIZE 512
+#define SOCKET_PORT "port-one"
+
+void ListenForMessage(int Socket, char * buffer)
+{
+  read( Socket, buffer, BUFFERSIZE );
+  buffer[strcspn( buffer, "\n" )] = 0;
+  printf( "Incoming:  %s\n", buffer );
+  fflush( stdout );  
+  memset( buffer, 0, BUFFERSIZE );
+
+}
+
+int main(){
+
+    char CharBuffer[BUFFERSIZE] = {0}; 
+    int FileToRead = open( SOCKET_PORT, O_RDONLY );
+    while ( 1 )
+    { 
+
+      ListenForMessage(FileToRead, CharBuffer);
+
+    }
+}
