809e43e577e655afe8d5d765bb80f2f463fdf00c
Guillaume VdS <guillaume.vds.97@gmail.com>
2021-08-26 23:59:32 +0100

    adding some comments

--- C_Receive.c
+++ C_Receive.c
@@ -11,22 +11,46 @@
 #include <fcntl.h> // Contains file controls like O_RDWR
 #include <errno.h> // Error integer and strerror() function
 #include <unistd.h> // write(), read(), close()
+#include <stdlib.h>
 
 #define BUFFERSIZE 512
 #define SOCKET_PORT "port-one"
+#define DESTINATION_FILE "inputfile.txt"
+
+/*Takes */
+void writeToFile(char * textinput){
+  FILE *fileptr = fopen(DESTINATION_FILE, "a");
+  printf(" %s\n", strerror(errno));
+  if(fileptr == NULL)
+  {
+    printf("File-Error");
+    exit(1);
+  }
+  fprintf(fileptr, "\n%s", textinput );
+  fclose(fileptr);
+}
+
+/*Takes a file descriptor for a socket and reads up to the count of BUFFERSIZE,
+storing this into the buffer.
+
+*/
 
 void ListenForMessage( int fileDescriptor, char * buffer ){
   read( fileDescriptor, buffer, BUFFERSIZE );
   buffer[strcspn( buffer, "\n" )] = 0;
   printf( "Incoming:  %s\n", buffer );
-  fflush( stdout );  
-  memset( buffer, 0, BUFFERSIZE );
+  fflush( stdout ); /* Prints what's in the buffer as the while loop sometimes 
+  skips print statements. */
+  writeToFile(buffer);             //Writes contents of the buffer into a file.
+  memset( buffer, 0, BUFFERSIZE ); //Clears the buffer for the next operation
 }
 
+/**/
+
 int main(){
-    char CharBuffer[BUFFERSIZE] = {0}; 
+    char CharBuffer[BUFFERSIZE] = {0};
     int FileToRead = open( SOCKET_PORT, O_RDONLY );
-    while ( true ){ 
-      ListenForMessage(FileToRead, CharBuffer);      
+    while ( true ){
+      ListenForMessage(FileToRead, CharBuffer);
     }
 }
--- PythonInput.py
+++ PythonInput.py
@@ -4,63 +4,66 @@ import serial
 import time
 
 MAX_USER_INPUT_BUFFER_LENGTH = 512
-SOCKET_PORT = 'port-two'
+SOCKET_PORT = "port-two"
 BAUDRATE = 9600
 SERIAL_READ_TIMEOUT = 5
 
+
 def check_for_input():
-    """ Will take user input, checks if input does not exceed 512 characters
+    """Will take user input, checks if input does not exceed 512 characters
     This loop will execute for as long as input is invalid.
     Will return input through buffer variable
     """
 
     while True:
-        input_buffer = input( "Please enter input here: " )
-        if( len( input_buffer ) <= MAX_USER_INPUT_BUFFER_LENGTH ):
+        input_buffer = input("Please enter input here: ")
+        if len(input_buffer) <= MAX_USER_INPUT_BUFFER_LENGTH:
             return input_buffer
         else:
-            print( "Current input exceeds maximum allowed characters." )
+            print("Current input exceeds maximum allowed characters.")
 
-def send_data_over_serial( serial_port: serial.Serial, data_to_send: str ):
-    """ This function takes input from the buffer in main, converts this to
+
+def send_data_over_serial(serial_port: serial.Serial, data_to_send: str):
+    """This function takes input from the buffer in main, converts this to
     utf-8. The serial port is opened and the converted data will
     be written to it.
     """
 
-    serial_port.write( bytes( data_to_send, 'ascii' ) )
+    serial_port.write(bytes(data_to_send, "ascii"))
 
-def receive_data_over_serial( serial_port_receive: serial.Serial ) -> str:
-    """ This function takes a serial port object, retrieves information from the
+
+def receive_data_over_serial(serial_port_receive: serial.Serial) -> str:
+    """This function takes a serial port object, retrieves information from the
     socket (in the form of bytes) and returns a decoded string.
     """
 
-    while ( data_received := serial_port_receive.read( 512 ) ) ==  b'':
-        print( "timed out with no bytes" )
+    while (data_received := serial_port_receive.read(512)) == b"":  # fmt: no
+        print("timed out with no bytes")
         pass
-    return data_received.decode( 'ascii' )
+    return data_received.decode("ascii")
+
 
 def main():
-    
+
     try:
         serial_port_object = serial.Serial(
-            port=SOCKET_PORT,
-            baudrate=BAUDRATE,
-            timeout = SERIAL_READ_TIMEOUT
+            port=SOCKET_PORT, baudrate=BAUDRATE, timeout=SERIAL_READ_TIMEOUT
         )
 
-        while True:    #Executes an infinite loop, to repeat below functions
+        while True:  # Executes an infinite loop, to repeat below functions
             main_buffer = check_for_input()
-            send_data_over_serial( serial_port_object, main_buffer )
-            print( receive_data_over_serial( serial_port_object ) )
+            send_data_over_serial(serial_port_object, main_buffer)
+            print(receive_data_over_serial(serial_port_object))
 
     except serial.SerialException as e:
-        print( "Error configuring Serial port" )
-        print( e )
+        print("Error configuring Serial port")
+        print(e)
     except Exception as e:
-        print( "Failed to perform action" )
-    #except KeyBoardInterrupt:
+        print("Failed to perform action")
+        # except KeyBoardInterrupt:
         serial_port.close()
         quit()
+
 
 if __name__ == "__main__":
     main()
